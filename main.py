be30d14a2e3c4624f388d9007b0db6d2c41e5b4f
LaurensDijkstra1 <laurensdijkstra1@gmail.com>
2020-11-11 23:34:44 +0100

    created main frame of the dashboard

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import tkinter as tk
+import serial
 
+class Application(tk.Frame):
+    """
+    The class that create the whole Sunshine application.
+    """
+
+    def __init__(self, window):
+        """
+        The initialization of the Sunshine application.
+
+        :param window:
+        """
+        super().__init__(window)  # constructor frame
+
+        # arduino
+        # self.arduino = serial.Serial('COM3', 19200)
+
+        # sidebar
+        self.sidebar = tk.Frame(self, relief=tk.RAISED, bd=2)
+        self.createSidebarContent()
+        self.radioValue = tk.StringVar()
+
+        # main
+        self.main = tk.Frame(self, relief=tk.RAISED, bd=2)
+        self.createMainContent()
+        # self.setTemp()
+
+        self.pack()
